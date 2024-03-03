7ef2758b5c4ff5837b555bb6aa727e2c80aae7a8
audric13420081 <88325147+audric13420081@users.noreply.github.com>
2024-03-03 21:55:08 +0700

    Update Model_Churn_v1.py

--- Model_Churn_v1.py
+++ Model_Churn_v1.py
@@ -76,8 +76,9 @@ def process_data(df_data_akun, df_trx, df_tutup_rek, is_training_data=True):
     loan_type_group_agg = loan_type_group_dummies.groupby(df_data_akun['nama_nasabah']).max()
 
     # Gabungkan semua DataFrame yang telah di-agregasi
-    df_data_akun_final = pd.concat([segmen_agg, rgdesc_agg, platform_channel_agg, giro_type_group_agg, loan_type_group_agg, df_data_akun['Average of Ratas Giro']], axis=1)
+    df_data_akun_final = pd.concat([segmen_agg, rgdesc_agg, platform_channel_agg, giro_type_group_agg, loan_type_group_agg], axis=1)
     df_data_akun_final.reset_index(inplace=True)  # Opsional: Reset index jika Anda ingin 'nama_nasabah' sebagai kolom
+    df_data_akun_final = pd.merge(df_data_akun_final, df_data_akun['Average of Ratas Giro], on='nama_nasabah', how='inner')
     df_data_akun_final
     
     # Konversi bulan_transaksi menjadi format tanggal
