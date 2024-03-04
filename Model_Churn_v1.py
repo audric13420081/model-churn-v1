c3a8db14ce3e8b6246b8be81dff7f1cb2f460fc1
audric13420081 <88325147+audric13420081@users.noreply.github.com>
2024-03-04 10:06:10 +0700

    Update Model_Churn_v1.py

--- Model_Churn_v1.py
+++ Model_Churn_v1.py
@@ -463,7 +463,9 @@ if uploaded_data_pred is not None:
         st.dataframe(tabel_urut_profitabilitas)
         
         # Menghitung matriks korelasi
-        correlation_matrix = analisis_data.corr()
+        analisis_data_dropped = analisis_data.drop(['Other','ratas_trx_january','ratas_trx_february','ratas_trx_march','ratas_trx_april','ratas_trx_may','ratas_trx_june','ratas_trx_july','vol_trx_january','vol_trx_february','vol_trx_march','vol_trx_april','vol_trx_may','vol_trx_june','vol_trx_july','frek_trx_january','frek_trx_february','frek_trx_march','frek_trx_april','frek_trx_may','frek_trx_june','frek_trx_july'], axis=1, inplace=True)
+ 
+        correlation_matrix = analisis_data_dropped.corr()
         
         # Visualisasi heatmap korelasi
         st.write("### Correlation Heatmap antar Fitur")
